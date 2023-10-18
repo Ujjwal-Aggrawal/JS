2b8b2f7917672634c200a911c501eb0ec81f6b9b
Ujjwal-Aggrawal <147369962+Ujjwal-Aggrawal@users.noreply.github.com>
2023-10-18 14:53:45 +0530

    Add files via upload

---
+++ begin.js
@@ -0,0 +1,12 @@
+// const Name = "Ujjwal"
+// let age = '23'
+// var surname = "Aggrawal"
+
+// // Name = "Goku" read notes(1.1)
+
+
+// // let age = '1000' read notes (1.2)
+
+// console.log(Name)
+// console.table([Name, age , surname])
+
